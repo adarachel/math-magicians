8627b7ef0b797e8935256c2d25c263c9bc0c2919
adarachel <adarachelofficial@gmail.com>
2023-04-14 14:26:31 +0100

    full website

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Routes, Link,
+} from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
-import './App.css';
 import Quotes from './components/Quote';
+import './navbar.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Quotes />
-      <Calculator />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <nav className="navbar">
+      {/* use class name */}
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/calculator">Calculator</Link>
+        </li>
+        <li>
+          <Link to="/quote">Quotes</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quotes />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
